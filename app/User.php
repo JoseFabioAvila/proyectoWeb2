06f0cdbc0de3837b391b6057a708b589937c3ab6
Jose Fabio Avila Estrada <fabioavila-16@hotmail.com>
2016-11-18 15:03:06 -0600

    habilitado agregado

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'esAdmin' 
+        'name', 'email', 'password', 'esAdmin','habilitado',
     ];
 
     /**
